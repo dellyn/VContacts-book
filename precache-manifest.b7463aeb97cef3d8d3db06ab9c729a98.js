85f6f4b76a00dd4de9ea7559bdea1ef0844e0b13
Igor Makarchuk <ingvardellyn@gmail.com>
2020-10-19 13:50:46 +0300

    Updates

---
+++ precache-manifest.b7463aeb97cef3d8d3db06ab9c729a98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3a8f444f8a5a3caeda2db7bb01fe1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "161c4c32a746ad7707b1",
+    "url": "/static/css/main.04381f1d.chunk.css"
+  },
+  {
+    "revision": "f0c692101ea1451e9962",
+    "url": "/static/js/2.b769a106.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b769a106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "161c4c32a746ad7707b1",
+    "url": "/static/js/main.4fa9a513.chunk.js"
+  },
+  {
+    "revision": "5f686874f5a758cd55cd",
+    "url": "/static/js/runtime-main.17746625.js"
+  }
+]);
